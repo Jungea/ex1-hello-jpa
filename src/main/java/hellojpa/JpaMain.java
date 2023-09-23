583ca8db3e2d57815bf0f6f2a0a257b37b5a4de5
Jungea <jungea2241@gmail.com>
2023-09-23 20:28:35 +0900

    준영속 상태

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,10 +16,13 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member(200L, "member200");
-            em.persist(member);
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAAAA");
 
-            em.flush();
+//            em.detach(member);
+            em.clear();
+
+            Member member1 = em.find(Member.class, 150L);
 
             System.out.println("===========================");
             tx.commit();
